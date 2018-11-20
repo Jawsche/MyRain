50fa7e7c903705c124c3db8b0a018d95576dc2e7
Jawsche <joshua.allin@live.com>
2018-11-21 08:13:52 +0900

    Polished Jump
    Added Variable Jump height and speed

--- MyRain/Assets/Scripts/PlayerControl.cs
+++ MyRain/Assets/Scripts/PlayerControl.cs
@@ -7,73 +7,72 @@ using UnitySampleAssets.CrossPlatformInput;
 public class PlayerControl : MonoBehaviour {
 
     //attributes
-    public float a_maxSpeed = 5.0f;
-
+    [Range (1, 10)]
+    public float a_maxSpeed = 8.5f;
+    [Range(1, 10)]
     public float a_jumpStrangth = 5.0f;
+    [Range(1, 10)]
+    public float fallMultiplier = 2.5f;
+    [Range(1, 10)]
+    public float lowJumpMultiplier = 2f;
+
+
 
     
 
     //internal variables
     private Rigidbody2D p_rb;
-    private Collider2D p_collider;
+    private CircleCollider2D p_colliderBase;
+    private BoxCollider2D p_colliderHead;
     private bool p_grounded = false;
 
-    //Debug Variables
-    public Vector2 VelTest;
 
 
-    void Start () {
+    private void Awake()
+    {
         p_rb = GetComponent<Rigidbody2D>();
-        p_collider = GetComponent<Collider2D>();
-	}
+        p_colliderBase = GetComponent<CircleCollider2D>();
+        p_colliderHead = GetComponent<BoxCollider2D>();
+    }
+
 	
 	void Update () {
         //horizontal input float
         float h = CrossPlatformInputManager.GetAxis("Horizontal");
 
+        //call grounding method
+        CheckGrounded();
+
         //Run while grounded
-        if (h != 0 && p_grounded)
+        if (h != 0)// && p_grounded)
         {
-            Vector2 temp;
-            temp = p_rb.velocity;
-            temp.x = a_maxSpeed * h;
-            p_rb.velocity = temp;
+            p_rb.velocity = new Vector2(h * a_maxSpeed, p_rb.velocity.y);
         }
 
-        //mid-air movement
-        if (h != 0 && !p_grounded)
-            p_rb.AddForce(Vector2.right * h * a_maxSpeed, ForceMode2D.Force);
-
-        //Basic Jump
+        //Jump
         if (CrossPlatformInputManager.GetButtonDown("Jump") && p_grounded)
             p_rb.AddForce(Vector2.up * a_jumpStrangth, ForceMode2D.Impulse);
-
-
-        ////Debug Velocity
-        //VelTest = p_rb.velocity;
-        //Debug.Log(VelTest);
+        if (p_rb.velocity.y < 0)
+            p_rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1f) * Time.deltaTime;
+        else if (p_rb.velocity.y > 0.0f && !CrossPlatformInputManager.GetButton("Jump"))
+            p_rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1f) * Time.deltaTime;
     }
 
-    void FixedUpdate()
+    private void CheckGrounded()
     {
-
-        //maxspeed while moving
-        if (p_rb.velocity.magnitude > a_maxSpeed && p_grounded)
-        {
-            p_rb.velocity = p_rb.velocity.normalized * a_maxSpeed;
-        }
-
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 0.6f);
-        //Debug.DrawLine(transform.position, transform.up * -0.6f, Color.red);
+        //Set Grounded variable
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 1.1f);
         if (hit.collider != null)
         {
             p_grounded = true;
+            Debug.DrawRay(transform.position, Vector3.down * 1.1f, Color.red);
             Debug.Log(hit.collider.name);
         }
         else
         {
             p_grounded = false;
         }
-        
     }
+
+ 
 }
