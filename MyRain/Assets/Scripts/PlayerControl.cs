d9ae30317ab9d0232a758a42e260751c7ff7b619
Jawsche <joshua.allin@live.com>
2018-11-21 20:37:37 +0900

    Added Crouching and basic Soft Body
    Plus a shitty walk cycle

--- MyRain/Assets/Scripts/PlayerControl.cs
+++ MyRain/Assets/Scripts/PlayerControl.cs
@@ -16,6 +16,8 @@ public class PlayerControl : MonoBehaviour {
     [Range(1, 10)]
     public float lowJumpMultiplier = 2f;
 
+    public TopChucnk topChunck;
+
 
 
     
@@ -24,7 +26,12 @@ public class PlayerControl : MonoBehaviour {
     private Rigidbody2D p_rb;
     private CircleCollider2D p_colliderBase;
     private BoxCollider2D p_colliderHead;
+    private Animator p_anim;
+    private float p_facingDir = 1.0f; 
     private bool p_grounded = false;
+    private bool p_standing = true;
+    private bool p_isWalking = false;
+    public float a_maxSpeedInit;
 
 
 
@@ -33,21 +40,35 @@ public class PlayerControl : MonoBehaviour {
         p_rb = GetComponent<Rigidbody2D>();
         p_colliderBase = GetComponent<CircleCollider2D>();
         p_colliderHead = GetComponent<BoxCollider2D>();
+        p_anim = GetComponent<Animator>();
+        a_maxSpeedInit = a_maxSpeed;
     }
 
-	
-	void Update () {
+
+    void Update()
+    {
         //horizontal input float
         float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        float v = CrossPlatformInputManager.GetAxis("Vertical");
 
         //call grounding method
         CheckGrounded();
+
+        //Update Animator
+        UpdateAnimator(h);
 
         //Run while grounded
         if (h != 0)// && p_grounded)
         {
             p_rb.velocity = new Vector2(h * a_maxSpeed, p_rb.velocity.y);
+            p_isWalking = true;
         }
+        else p_isWalking = false;
+
+        if (h < 0.0f)
+            p_facingDir = -1.0f;
+        if (h > 0.0f)
+            p_facingDir = 1.0f;
 
         //Jump
         if (CrossPlatformInputManager.GetButtonDown("Jump") && p_grounded)
@@ -56,8 +77,34 @@ public class PlayerControl : MonoBehaviour {
             p_rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1f) * Time.deltaTime;
         else if (p_rb.velocity.y > 0.0f && !CrossPlatformInputManager.GetButton("Jump"))
             p_rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1f) * Time.deltaTime;
-    }
 
+        //Crouch
+        if (CrossPlatformInputManager.GetButtonDown("Fire1") && p_standing == true)
+        {
+            p_standing = false;
+            float tempX;
+            float tempY;
+            tempX = topChunck.relativeOffset.y * p_facingDir;
+            tempY = topChunck.relativeOffset.x;
+            topChunck.relativeOffset = new Vector2(tempX, tempY);
+            if (p_facingDir == 1.0f)
+                transform.eulerAngles = new Vector3(0.0f, 0.0f, -90.0f);
+            else if (p_facingDir == -1.0f)
+                transform.eulerAngles = new Vector3(0.0f, 0.0f, 90.0f);
+            a_maxSpeed *= 0.7f;
+        }
+        //stand back up
+        else if (CrossPlatformInputManager.GetButtonDown("Fire1") && p_standing == false)
+        {
+            p_standing = true;
+            topChunck.relativeOffset = topChunck.relativeOffsetInit;
+            if (p_facingDir == 1.0f)
+                transform.eulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
+            else if (p_facingDir == -1.0f)
+                transform.eulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
+            a_maxSpeed = a_maxSpeedInit;
+        }
+    }
     private void CheckGrounded()
     {
         //Set Grounded variable
@@ -66,13 +113,19 @@ public class PlayerControl : MonoBehaviour {
         {
             p_grounded = true;
             Debug.DrawRay(transform.position, Vector3.down * 1.1f, Color.red);
-            Debug.Log(hit.collider.name);
+            //Debug.Log(hit.collider.name);
         }
         else
         {
             p_grounded = false;
         }
     }
+    private void UpdateAnimator(float h)
+    {
+        p_anim.SetBool("IsWalking", p_isWalking);
+        p_anim.SetFloat("Horizontal", h);
+    }
 
- 
+
+
 }
---
+++ MyRain/Assets/Scripts/TopChucnk.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopChucnk : MonoBehaviour
+{
+
+    public Transform parentNode;
+    public Vector2 relativeOffset;
+    public Vector2 relativeOffsetInit;
+    public float returnSpeed = 15.0f;
+
+    // Use this for initialization
+    void Awake()
+    {
+        relativeOffsetInit = relativeOffset;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.eulerAngles = new Vector2(Mathf.Lerp(transform.eulerAngles.x, parentNode.localEulerAngles.x, Time.deltaTime * returnSpeed), Mathf.Lerp(transform.eulerAngles.y, parentNode.localEulerAngles.y, Time.deltaTime * returnSpeed));
+        transform.position = new Vector2(Mathf.Lerp(transform.position.x, parentNode.position.x + relativeOffset.x, Time.deltaTime * returnSpeed), Mathf.Lerp(transform.position.y, parentNode.position.y + relativeOffset.y, Time.deltaTime * returnSpeed));
+    }
+}
